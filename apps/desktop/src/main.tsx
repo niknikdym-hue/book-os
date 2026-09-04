6a3f0c237b8bf25fdbc1f6bf546e124036463724
niknikdym-hue <niknikdym@gmail.com>
2026-09-04 21:23:46 +0300

    Task 014: fix frontend readiness narrowing

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -4,10 +4,11 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 
-const rootElement = document.getElementById("root");
-if (!rootElement) {
+const rootCandidate = document.getElementById("root");
+if (!rootCandidate) {
   throw new Error("BOOK OS root element is missing");
 }
+const rootElement: HTMLElement = rootCandidate;
 
 createRoot(rootElement).render(
   <StrictMode>
