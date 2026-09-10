0b1da1a8079085d6a08483564e67eb179fe7e71d
niknikdym-hue <niknikdym@gmail.com>
2026-09-10 09:02:51 +0300

    Launch UX: load author studio visual system

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 import "./launchUx.css";
+import "./studio.css";
 
 const rootCandidate = document.getElementById("root");
 if (!rootCandidate) {
